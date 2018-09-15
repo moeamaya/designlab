5351ad18eed98f4256932c9b7a417faf24374193
ElisaR <eli.gort@yahoo.it>
2018-09-15 20:01:33 +0200

    Second section

---
+++ main.js
@@ -0,0 +1,21 @@
+
+// ................sticky elements.....................
+
+// var topics = document.querySelector('.topics-graphic')
+// var topicsPosition = topics.getBoundingClientRect()
+// // var placeholder = document.createElement('div')
+// // placeholder.style.width = topicsPosition.width + 'px'
+// // placeholder.style.height = topicsPosition.height + 'px'
+// var topicsIsSticky = false
+//
+// window.addEventListener('scroll', function() {
+//     if (window.pageYOffset >= topicsPosition.top && !topicsIsSticky) {
+//         topics.classList.add('sticky')
+//         // topics.parentNode.insertBefore(placeholder, topics)
+//         topicsIsSticky = true
+//     } else if (window.pageYOffset < topicsPosition.top && isAdded) {
+//         topics.classList.remove('sticky')
+//         // topics.parentNode.removeChild(placeholder)
+//         topicsIsSticky = false
+//     }
+// })
