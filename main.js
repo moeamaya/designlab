16e15818ccf284174e0b8db379905a4430072eb7
elisa <eli.gort@yahoo.it>
2018-09-17 17:39:25 +0200

    stickybits integration

--- main.js
+++ main.js
@@ -2,7 +2,9 @@
 // ................sticky elements.....................
 
 // var topics = document.querySelector('.topics-graphic')
+// var col = document.querySelector('.topic-col')
 // var topicsPosition = topics.getBoundingClientRect()
+// var colPosition = col.getBoundingClientRect()
 // // var placeholder = document.createElement('div')
 // // placeholder.style.width = topicsPosition.width + 'px'
 // // placeholder.style.height = topicsPosition.height + 'px'
@@ -11,11 +13,27 @@
 // window.addEventListener('scroll', function() {
 //     if (window.pageYOffset >= topicsPosition.top && !topicsIsSticky) {
 //         topics.classList.add('sticky')
+//         topics.style.width = colPosition.width + 'px'
 //         // topics.parentNode.insertBefore(placeholder, topics)
 //         topicsIsSticky = true
-//     } else if (window.pageYOffset < topicsPosition.top && isAdded) {
+//     } else if (window.pageYOffset < topicsPosition.top && topicsIsSticky) {
 //         topics.classList.remove('sticky')
 //         // topics.parentNode.removeChild(placeholder)
 //         topicsIsSticky = false
 //     }
 // })
+
+function isIE() {
+  ua = navigator.userAgent;
+  /* MSIE used to detect old browsers and Trident used to newer ones*/
+  var is_ie = ua.indexOf("MSIE ") > -1 || ua.indexOf("Trident/") > -1;
+  return is_ie;
+}
+
+var stickybit = stickybits('.topics-graphic', { useStickyClasses: true })
+
+if (isIE()){
+    window.addEventListener('resize', function() {
+      stickybit.update()
+    })
+}
